6e97381c2acecccdc6df71f7c317ec618c7490f2
sudo-apt-get-good <89986144+toldeveloper@users.noreply.github.com>
2022-09-22 22:11:34 +0200

    import done-ish

---
+++ script.js
@@ -0,0 +1,28 @@
+async function getData() {
+    const url = "https://wordpress.toldeveloper.dk/wp-json/wp/v2/posts?_embed";
+    const res = await fetch(url);
+    const data = await res.json();
+    appendPosts(data);
+}
+
+getData();
+
+// append wp posts to the DOM
+function appendPosts(posts) {
+    let htmlTemplate = "";
+    for (let post of posts) {
+        console.log(post);
+        htmlTemplate += /*html*/ `
+      <article class="portfolioPost">
+        <div>
+            <img src="${post.acf.myimagelabel.url}">
+        </div>
+        <div>
+            <h3>${post.title.rendered}</h3>
+            <p>${post.content.rendered}</p>
+        </div>
+      </article>
+    `;
+    }
+    document.querySelector(".portfolio_insert").innerHTML = htmlTemplate;
+}
