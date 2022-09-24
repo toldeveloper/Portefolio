d61c1bac093ff4197d6935e067aa43bd181762f4
sudo-apt-get-good <89986144+toldeveloper@users.noreply.github.com>
2022-09-24 19:41:13 +0200

    form virker!

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Wordpress start here ------------------------------------
+
 async function getData() {
     const url = "https://wordpress.toldeveloper.dk/wp-json/wp/v2/posts?_embed";
     const res = await fetch(url);
@@ -31,6 +33,31 @@ function appendPosts(posts) {
 }
 
 
-
+// Activate onscroll animations -------------------------------------
 
   AOS.init();
+
+
+// EmailJS kontaktformular ----------------------------------------
+
+(function() {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init('_I26l-u9I7qmc80Fe');
+})();
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = Math.random() * 100000 | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm('contact_service', 'contact_form', this)
+          .then(function() {
+              console.log('SUCCESS!');
+              alert("Mail sendt! \n\nJeg vil forsøge at svare dig hurtigst muligt");
+          }, function(error) {
+              console.log('FAILED...', error);
+              alert("Noget gik galt..\n\nKontakt mig eventuelt igennem LinkedIN i stedet");
+          });
+  });
+}
